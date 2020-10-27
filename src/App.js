ae7e11a6a14911681bff38a438e5af3a5e3a90b3
Paulo Tarcio <pvtarcio@gmail.com>
2020-10-27 14:43:01 -0400

    clone da netflix finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Tmdb from './Tmdb';
+import Header from './components/Header/Header';
+import MovieRow from './components/MovieRow';
+import FeatureMove from './components/FeatureMovie';
 
-function App() {
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      let originals = list.filter((i) => i.slug === 'originals');
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
+    };
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Header black={blackHeader} />
+
+      {featuredData && <FeatureMove item={featuredData} />}
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      <footer>
+        Feito com{' '}
+        <span role="img" aria-label="coracao" >
+          S2
+        </span>
+        <br />
+        Direito de iamgem para Netflix <br />
+        Dados pego do site TheMovieDB.org
+      </footer>
+
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img src="http://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif"  alt=""/>
+        </div>
+      )}
     </div>
   );
-}
-
-export default App;
+};
